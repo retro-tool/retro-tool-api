ad5b728372216ed2c9d277e85f2c4bbafca483b7
uesteibar <uesteibar@gmail.com>
2020-04-26 03:17:27 +0200

    Add ecto query metrics to phoenix dashbaord

--- lib/xrt_web/schema.ex
+++ lib/xrt_web/schema.ex
@@ -5,32 +5,24 @@ defmodule XrtWeb.Schema do
 
   use Absinthe.Schema
 
-  import_types(XrtWeb.Types.{
-    User,
-    Retro
-  })
+  import_types(XrtWeb.Types.User)
+  import_types(XrtWeb.Types.Retro)
 
-  import_types(XrtWeb.Schemas.Queries.{
-    User,
-    Retro
-  })
+  import_types(XrtWeb.Schemas.Queries.User)
+  import_types(XrtWeb.Schemas.Queries.Retro)
 
   query do
     import_fields(:user_queries)
     import_fields(:retro_queries)
   end
 
-  import_types(XrtWeb.Schemas.Mutations.{
-    Retro
-  })
+  import_types(XrtWeb.Schemas.Mutations.Retro)
 
   mutation do
     import_fields(:retro_mutations)
   end
 
-  import_types(XrtWeb.Schemas.Subscriptions.{
-    Retro
-  })
+  import_types(XrtWeb.Schemas.Subscriptions.Retro)
 
   subscription do
     import_fields(:retro_subscriptions)
