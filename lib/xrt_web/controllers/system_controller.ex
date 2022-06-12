69466517b79318fea632155ac969a4d48e03dd6f
Andrzej Trzaska <atrzaska2@gmail.com>
2022-06-12 19:39:49 +0200

    Fix spec name

--- lib/xrt_web/controllers/system_controller.ex
+++ lib/xrt_web/controllers/system_controller.ex
@@ -1,7 +1,7 @@
 defmodule XrtWeb.SystemController do
   use XrtWeb, :controller
 
-  @spec run(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  @spec index(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def index(conn, _params) do
     text(conn, "OK")
   end
